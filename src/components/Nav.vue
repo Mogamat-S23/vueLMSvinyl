1f3ee02a2d02bff2da94a7f081a3f83a10dbf7d4
Mogamat-S23 <saleemariefdien@gmail.com>
2022-08-11 20:29:54 +0200

    sixth push

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,6 +4,8 @@
     <router-link to="/allproducts">Vinyles</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact Us</router-link> |
+    <router-link to="/register">Register</router-link>  |
+    <router-link to="/login">Login</router-link>
     
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
-  }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
 ]
 
 const router = createRouter({
@@ -39,3 +55,5 @@ const router = createRouter({
 })
 
 export default router
+
+
